e2e0e31dc39244d1b351b190e857ae1c6f503098
Rodrigo Fernandes <rfernandes@digital-works.com>
2020-09-15 01:51:58 +0100

    stylized questions list

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,10 +1,15 @@
 import React, { Router } from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import Grow from "@material-ui/core/Grow";
 import ListItemText from "@material-ui/core/ListItemText";
 import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowLeftRoundedIcon from "@material-ui/icons/ArrowLeftRounded";
+import ArrowRightRoundedIcon from "@material-ui/icons/ArrowRightRounded";
+import Button from '@material-ui/core/Button'; 
+import LinearProgress from "@material-ui/core/LinearProgress";
 import "../App.css";
 
 function generateElements() {
@@ -12,7 +17,11 @@ function generateElements() {
 
   for (let index = 0; index < 20; index++) {
     elementsArray.push(
-      <Grow in style={{ transformOrigin: "0 0 0" }} {...{ timeout: index * (2000 / 20) }}>
+      <Grow
+        in
+        style={{ transformOrigin: "0 0 0" }}
+        {...{ timeout: index * (5000 / 20) }}
+      >
         <ListItem button>
           <ListItemText primary="Um texto" secondary="Secondary text" />
         </ListItem>
@@ -26,11 +35,28 @@ function generateElements() {
 function Home() {
   return (
     <div>
-      <Grid>
-        <List>
-          {generateElements()}
-        </List>
-      </Grid>
+      {/* <div className="progress-wrapper">
+        <LinearProgress color="secondary" className="progress" />
+      </div> */}
+
+      <Container className="container" spacing={10}>
+        <Grid container className="list-container">
+          <Grid item xs={12} sm={12} className="grid-item grid-list">
+            <List wrap className="list" style={{ padding: 0 }}>
+              {generateElements()}
+            </List>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container>
+        <IconButton >
+          <ArrowLeftRoundedIcon className="control-btn"/>
+        </IconButton>
+        <Button className="control-btn">1</Button><Button className="control-btn">2</Button>
+        <IconButton >
+          <ArrowRightRoundedIcon className="control-btn"/>
+        </IconButton>
+      </Container>
     </div>
   );
 }
